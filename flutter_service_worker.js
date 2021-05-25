bc513112f37f5b2660b4f86aa1ab8fed236aea03
7wataaa <7wataaa@users.noreply.github.com>
2021-05-25 07:01:38 +0000

    deploy: 586396c600a2665d50edf8a2e8bbcbbc6b086116

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "0d93c3158fb9ec759eb12945d763641a",
-"/": "0d93c3158fb9ec759eb12945d763641a",
+"index.html": "f09cbefd1cc3bba44dcb7fa736619994",
+"/": "f09cbefd1cc3bba44dcb7fa736619994",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
